35a3325216f71f1c442a011233e0ea069f7e23f3
Vivek Kumar Chaudhary <chaudharv101@gmail.com>
2020-04-10 17:57:02 +0530

    make index

--- header.php
+++ header.php
@@ -13,22 +13,4 @@
         include './class/user.php';
         $user_obj = new User();
         ?>
-
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#"></a>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="index.php">Home</a></li>
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
+        <a href="index.php">Home</a>
